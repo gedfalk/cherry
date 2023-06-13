fac040cb21e22363e237af1ef67f81c0e75a5dca
gedfalk <blite.co.stn@gmail.com>
2023-06-13 18:24:17 +0300

    init: Create template for interactive menu
    Creating first type of interactive mode.

--- cherryCore/menuExample.py
+++ cherryCore/menuExample.py
@@ -3,29 +3,35 @@
 
 from rich.console import Console
 from readchar import readkey, key
+from time import sleep
 
 def main():
-    console = Console()
+    console = Console(width=27)
 
     menu = ['Start', 'Pause', 'Refresh', 'Exit']
     currentOption = 0
 
     while True:
         console.clear()
-        
+       
+        console.rule(f'[bold blue]Cherry')
         for i, option in enumerate(menu):
             if currentOption == i:
-                console.print(f'[green]{option}[/green]')
+                console.print(f'[bold green]  {option}[/bold green]')
             else:
-                console.print(f'{option}')
+                console.print(f' {option}')
+        console.rule()
 
         k = readkey()
-        if k == key.DOWN:
+        if k == key.DOWN or k == 'k':
             currentOption = (currentOption + 1) % len(menu)
-        elif k == key.UP:
+        elif k == key.UP or k == 'i':
             currentOption = (currentOption - 1) % len(menu)
-        elif k == key.ENTER and currentOption == 3:
+        elif (k == key.ENTER or k == key.SPACE) and currentOption == 3:
+            console.clear()
             break
+
+        
      
 
 if __name__ == '__main__':
