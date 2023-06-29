b2fbc01b6f0482f17f702ce43f798b292f78717f
gedfalk <blite.co.stn@gmail.com>
2023-06-29 14:37:19 +0300

    refactor: configuring properly Timer.run()

--- cherryCore/timer.py
+++ cherryCore/timer.py
@@ -6,23 +6,45 @@ from cherryCore.cherry import CherrySession
 
 from datetime import datetime, timedelta
 
-
 class Timer():
     def __init__(self, console):
         self.cherrySession = CherrySession()
+        self.bufTime = {
+            key: None for key in [  
+                'startTime', 
+                'totalTime', 
+                'endTime', 
+                'currentTime', 
+                'formattedTime',
+                ]
+            }
         self.console = console
 
     def run(self):
+        self.activateMode()
         # required for storing data later
         startTime = datetime.now()
         totalTime = timedelta(minutes=self.cherrySession.focusTime)
-        endTime = startTime + totalTime 
+        endTime = startTime + totalTime
+        self.bufTime['startTime'] = startTime
+        self.bufTime['totalTime'] = totalTime
+        self.bufTime['endTime'] = endTime
         while datetime.now() < endTime:
             currentTime = endTime - datetime.now()
             dummyTime = datetime(1, 1, 1) + currentTime
             formattedTime = dummyTime.strftime('%M:%S')
-            self.console.print(formattedTime, end='\r')
+
+
+            self.display(formattedTime)
             sleep(1)
+
+    def display(self, formattedTime):
+        self.console.print(f'   {formattedTime}', end='\r')
+
+
+    def activateMode(self):
+        self.console.print()
+
 
 
 def main():
