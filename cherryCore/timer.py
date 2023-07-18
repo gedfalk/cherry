6c23a49429f7104ebbed27286c2316ef55c52f79
gedfalk <blite.co.stn@gmail.com>
2023-07-18 18:18:23 +0300

    refactor: Finishing pomodoro-core-functionality in timer.py

--- cherryCore/timer.py
+++ cherryCore/timer.py
@@ -11,6 +11,9 @@ import tty, termios
 import select
 
 
+_DEV_ = 1
+_DEV_ = 1000
+
 
 def aread_key():
     inp, _, _ = select.select([sys.stdin], [], [], 0)
@@ -25,6 +28,7 @@ class Timer():
     def __init__(self, currentMode):
         self.old_terminal_settings = termios.tcgetattr(sys.stdin)
         self.cherrySession = CherrySession()
+        self.finished = False
         self.running = False
         self.bufTime = {
             key: None for key in [  
@@ -41,16 +45,15 @@ class Timer():
         self.console = Console()
 
     # @staticmethod
-    def raw_mode_on(func):
+    def raw_mode_on_and_off(func):
         def wrapper(self, *args, **kwargs):
             tty.setraw(sys.stdin.fileno())
             result = func(self, *args, **kwargs)
             termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.old_terminal_settings)
             return result
         return wrapper
-
     # @staticmethod
-    def raw_mode_off(func):
+    def raw_mode_off_and_on(func):
         def wrapper(self, *args, **kwargs):
             termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.old_terminal_settings)
             result = func(self, *args, **kwargs)
@@ -59,46 +62,57 @@ class Timer():
         return wrapper
 
 
-    @raw_mode_on
+    @raw_mode_on_and_off
     def run(self):
         self.running = True
+        # currentTime = timedelta(minutes=self.cherrySession.focusTime)
         self.activateMode()
         # required for storing data later
-        startTime = datetime.now()
-        totalTime = timedelta(minutes=self.cherrySession.focusTime)
-        endTime = startTime + totalTime
-        self.bufTime['startTime'] = startTime
-        self.bufTime['totalTime'] = totalTime
-        self.bufTime['endTime'] = endTime
+        self.bufTime['startTime'] = datetime.now()
 
-        while datetime.now() < endTime:
-            if self.running:
-                currentTime = endTime - datetime.now()
-                dummyTime = datetime(1, 1, 1) + currentTime
-                formattedTime = dummyTime.strftime('%M:%S')
-                self.display(formattedTime)
-
-            key = aread_key()
-            if key == 'q':
-                self.displayAnything('Timer has been stopped.\nSee you later)\n')
-                break
-            elif key == ' ':
-                self.running = not self.running
-            elif key == '\x03':
-                try:
-                    raise KeyboardInterrupt
-                except KeyboardInterrupt:
-                    self.displayAnything(f'Oh, you are leaving...\nWell, bye-bye then\n')
+        
+        for r in range(self.cherrySession.rounds):
+            currentTime = timedelta(minutes=self.cherrySession.focusTime)
+            self.displayAnything(f'Round {r+1}')
+            while currentTime.total_seconds() > 0:
+                    
+                # region Reading Keystrokes
+                key = aread_key()
+                if key == 'q':
+                    self.displayAnything('Timer has been stopped.\nSee you later)\n')
+                    # self.finished = True
+                    return
                     break
+                elif key == ' ':
+                    self.running = not self.running
+                elif key == '\x03':
+                    try:
+                        raise KeyboardInterrupt
+                    except KeyboardInterrupt:
+                        self.displayAnything(f'Oh, you are leaving...\nWell, bye-bye then\n')
+                        # self.finished = True
+                        return
+                        break
+                else:
+                # endregion
+                    if self.running:
+                        self.displayTime(currentTime)
+                        currentTime -= timedelta(milliseconds=100*_DEV_)
 
-            sleep(0.1)
+                    
+                sleep(0.1)
+        else:
+            self.displayAnything(f'Our work here is over... Well Done!)')
 
-    def display(self, formattedTime):
-        self.console.print(f'   {formattedTime}', end='\r')
+    @raw_mode_off_and_on
+    def displayTime(self, currentTime):
+        dummyTime = datetime(1, 1, 1) + currentTime
+        formattedTime = dummyTime.strftime('%M:%S')
+        self.console.print(f'_  {formattedTime}', end='\r')
 
-    @raw_mode_off
+    @raw_mode_off_and_on
     def displayAnything(self, anything):
-        self.console.print(f'{anything}       ')
+        self.console.print(f'{anything}   ')
 
     def activateMode(self):
         self.console.print()
@@ -107,7 +121,6 @@ class Timer():
 
 def main():
     a = Timer(Console())
-    a.cherrySession.printConfig()
     a.run()
 
 
