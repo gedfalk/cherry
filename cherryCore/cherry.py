970e870589b23d4bfb5d148460b29262d12defa7
gedfalk <blite.co.stn@gmail.com>
2023-07-25 19:43:57 +0300

    feature: implement writeConfig for saving timer_settings in cherry.conf

--- cherryCore/cherry.py
+++ cherryCore/cherry.py
@@ -15,27 +15,54 @@ def readConfig(configFile):
 
     return focusTime, breakTime, lbreakTime, rounds
 
+def writeConfig(configFile, timer_settings: tuple):
+    conf = configparser.ConfigParser()
+    conf.read(configFile)
+    
+    timerLabel = 'customTimer' + ''.join([f'{i:02}' for i in timer_settings])
+    if timerLabel not in conf:
+        frequency = 1
+    else:
+        frequency = int(conf[timerLabel]['frequency']) + 1
+    conf[timerLabel] = {}
+    
+    ft, bt, *dummy = timer_settings
+    r, lbt = None, None
+    conf[timerLabel]['focustime'] = str(ft)
+    conf[timerLabel]['breaktime'] = str(bt)
+    if len(timer_settings) > 2:
+        r = timer_settings[2]
+        conf[timerLabel]['rounds'] = str(r)
+    if len(timer_settings) == 4:
+        lbt = timer_settings[3]
+        conf[timerLabel]['lbreaktime'] = str(lbt)
+    conf[timerLabel]['frequency'] = str(frequency)
+
+    # TODO:
+    # Need to change [activeTimer] to either last used timer or most frequent
+
+    with open(configFile, 'w') as configWrite:
+        conf.write(configWrite)
+
+    return ft, bt, r, lbt
 
 
 
 
 class CherrySession():
     def __init__(self, timer_settings = None):
-        basic_settings = (30, 5, 4, 10)
-        ft, bt, r, lbt = basic_settings
+        
         if not timer_settings:
             # Need to change path to absolute?
             ft, bt, lbt, r = readConfig('cherryCore/cherry.conf')
         else:
-            ft, bt, *dummy = timer_settings
-            if len(timer_settings) > 2:
-                r = timer_settings[2]
-            if len(timer_settings) == 4:
-                lbt = timer_settings[3]
+            ft, bt, r, lbt = writeConfig('cherryCore/cherry.conf', timer_settings)
+            
         self.focusTime = ft
         self.breakTime = bt
+        self.rounds = r or 1
         self.lbreakTime = lbt
-        self.rounds = r
+
 
     def printConfig(self):
         print(f'\tCurrent config of the session looks like:')
